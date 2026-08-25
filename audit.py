c770c73cf73c2265d8641fed4c5c1d82e34f1ab8
KaiMonno <kmonno@oberlin.edu>
2026-08-25 15:26:30 -0700

    cleaned up repo and added gitignore

--- audit.py
+++ audit.py
@@ -4,4 +4,8 @@ url = input("Enter the URL to audit: ")
 response = requests.get(url)
 
 print(response.status_code)
-print(response.text[:500])
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.title)
