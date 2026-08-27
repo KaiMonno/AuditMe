7cf7c61a7650534e6b5c0321eb593354ca328596
KaiMonno <kmonno@oberlin.edu>
2026-08-27 13:53:48 -0700

    updated code

--- audit.js
+++ audit.js
@@ -1,5 +1,13 @@
 const { chromium } = require('playwright'); 
 (async () => { const browser = await chromium.launch(); 
-    const page = await browser.newPage(); await page.goto(process.argv[2]); 
+    const page = await browser.newPage();  
+    
+    page.on('response', response => {
+        console.log('Response:', response.status(), 'URL:', response.url())
+    })
+    page.on('error', error => {
+        console.log('Error:', error)
+    })
+    await page.goto(process.argv[2]);
     const title = await page.title(); console.log('Page title:', title); 
     await browser.close(); })();
